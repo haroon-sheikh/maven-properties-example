2b56596ee3293c8af7386416e501ab7a36e635a3
saiko <saiko@anterch>
2017-09-30 23:54:39 +0530

    Remove blank line

--- src/test/java/com/sitture/maven/properties/PropertiesTest.java
+++ src/test/java/com/sitture/maven/properties/PropertiesTest.java
@@ -11,8 +11,7 @@ public class PropertiesTest {
 		String prop1 = System.getProperty("my.Property");
 		String prop2 = System.getProperty("her.Property");
 		
-		System.out.println(prop1);
-		
+		System.out.println(prop1);		
 		System.out.println(prop2);
 		
 		Assert.assertNotNull(prop1);
