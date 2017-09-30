0b08116bb8d46ef9f45dabf2c28cf96a1d955196
saiko <saiko@anterch>
2017-09-30 23:50:41 +0530

    Fix test and add new env variable property

--- src/test/java/com/sitture/maven/properties/PropertiesTest.java
+++ src/test/java/com/sitture/maven/properties/PropertiesTest.java
@@ -2,11 +2,21 @@ package com.sitture.maven.properties;
 
 import org.junit.Test;
 
+import org.junit.Assert;
+
 public class PropertiesTest {
 
 	@Test
 	public void canGetProperty() {
-		System.out.println(System.getProperty("property.package"));
+		String prop1 = System.getProperty("my.Property");
+		String prop2 = System.getProperty("her.Property");
+		
+		System.out.println(prop1);
+		
+		System.out.println(prop2);
+		
+		Assert.assertNotNull(prop1);
+		Assert.assertNotNull(prop2);
 	}
 
 }
