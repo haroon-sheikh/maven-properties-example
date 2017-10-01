8b119c6bebbb264e2e579b94d2383ffc18c73a09
saiko <saiko@anterch>
2017-10-01 11:06:49 +0530

    Add new example

--- src/test/java/com/sitture/maven/properties/PropertiesTest.java
+++ src/test/java/com/sitture/maven/properties/PropertiesTest.java
@@ -10,12 +10,17 @@ public class PropertiesTest {
 	public void canGetProperty() {
 		String prop1 = System.getProperty("my.Property");
 		String prop2 = System.getProperty("her.Property");
-		
-		System.out.println(prop1);		
+		String prop3 = System.getProperty("pathvar");
+
+		// System.out.println("..........." + System.getenv("PATH"));
+
+		System.out.println(prop1);
 		System.out.println(prop2);
-		
+		System.out.println(prop3);
+
 		Assert.assertNotNull(prop1);
 		Assert.assertNotNull(prop2);
+		Assert.assertNotNull(prop3);
 	}
 
 }
